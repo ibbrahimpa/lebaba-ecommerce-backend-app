baea2cbb4bc465c98769c7afbc01e3dfc36a11f0
ibbrahimpa <ebrahimpanvelwala@gmail.com>
2025-02-27 20:48:20 +0530

    url update

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ app.use(cookieParser());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}))
 app.use(cors({
-    origin: 'https://lebaba-frontend-final-lime.vercel.app',
+    origin: 'https://lebaba-frontend-final-umber.vercel.app',
     credentials: true
 }))
 
